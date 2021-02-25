54fe7d20d64605c7abd65bbb527cd2fa2cc19fc2
chenyong <chenyong@hztianque.com>
2021-02-25 18:52:46 +0800

    完成三道波浪

---
+++ miniprogram/components/Wave/Wave.js
@@ -0,0 +1,167 @@
+// components/Wave.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        windowWidth: 0, // 屏幕宽度
+        ctx: '', // canvas实例对象
+        offsetX1: 0, // canvas波浪图向左偏移的量
+        offsetX2: 0, // canvas波浪图向左偏移的量
+        offsetX3: 0, // canvas波浪图向左偏移的量
+        interval: null, // 定时器
+    },
+
+    // 在组件实例进入页面节点树时执行
+    attached() {
+        // 获取设备信息
+        wx.getSystemInfo({
+            success: (res) => {
+                console.log(res)
+                this.data.windowWidth = res.windowWidth;
+            },
+        });
+        // 初始化canvas画布
+        this.canvasInit();
+    },
+
+    // 组件所在页面的生命周期
+    pageLifetimes: {
+        // 页面被展示
+        show: function () {
+            // 执行定时器
+            this.runInterval();
+        },
+
+        // 页面被隐藏
+        hide: function () {
+            // 清除定时器
+            clearInterval(this.data.interval)
+        },
+
+        // 页面尺寸变化
+        resize: function (size) {}
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        // 初始化canvas画布
+        canvasInit() {
+            this.ctx = wx.createCanvasContext('waveCanvas');
+        },
+
+        // 执行定时器
+        runInterval() {
+            this.data.interval = setInterval(() => {
+                this.data.offsetX1 = this.data.offsetX1 -= 6
+                if (this.data.offsetX1 <= -320) {
+                    this.data.offsetX1 = 0
+                }
+                this.data.offsetX2 = this.data.offsetX2 -= 4
+                if (this.data.offsetX2 <= -320) {
+                    this.data.offsetX2 = 0
+                }
+                this.data.offsetX3 = this.data.offsetX3 -= 1
+                if (this.data.offsetX3 <= -320) {
+                    this.data.offsetX3 = 0
+                }
+
+                // 执行绘制
+                this.runOneWave(this.data.offsetX1)
+                this.runTowWave(this.data.offsetX2)
+                this.runThreeWave(this.data.offsetX3)
+                this.ctx.draw()
+            }, 50)
+        },
+
+        // 绘制第一道波浪
+        runOneWave(offsetX) {
+            // console.log(offsetX)
+
+            this.ctx.beginPath()
+            // 绘制上波浪
+            for (let i = 0; i < 5; i++) {
+                this.ctx.moveTo(offsetX + i * 320, 70)
+                this.ctx.quadraticCurveTo(offsetX + i * 320 + 80, 55, offsetX + i * 320 + 160, 70)
+            }
+
+            // 绘制下波浪
+            for (let i = 0; i < 5; i++) {
+                this.ctx.moveTo(offsetX + i * 320 + 160, 70)
+                this.ctx.quadraticCurveTo(offsetX + i * 320 + 240, 85, offsetX + i * 320 + 320, 70)
+            }
+
+            // 绘制矩形封闭
+            this.ctx.lineTo(this.data.windowWidth, 300)
+            this.ctx.lineTo(0, 300)
+            this.ctx.lineTo(0, 70)
+            this.ctx.setGlobalAlpha(0.9)
+            this.ctx.fillStyle = "#fff"
+            this.ctx.fill()
+        },
+
+        // 绘制第二道波浪
+        runTowWave(offsetX) {
+            console.log(offsetX)
+
+            this.ctx.beginPath()
+            // 绘制上波浪
+            for (let i = 0; i < 5; i++) {
+                this.ctx.moveTo(offsetX + i * 320, 65)
+                this.ctx.quadraticCurveTo(offsetX + i * 320 + 80, 55, offsetX + i * 320 + 160, 65)
+            }
+
+            // 绘制下波浪
+            for (let i = 0; i < 5; i++) {
+                this.ctx.moveTo(offsetX + i * 320 + 160, 65)
+                this.ctx.quadraticCurveTo(offsetX + i * 320 + 240, 75, offsetX + i * 320 + 320, 65)
+            }
+
+            // 绘制矩形封闭
+            this.ctx.lineTo(this.data.windowWidth, 300)
+            this.ctx.lineTo(0, 300)
+            this.ctx.lineTo(0, 65)
+            this.ctx.setGlobalAlpha(0.6)
+            this.ctx.fillStyle = "#fff"
+            this.ctx.fill()
+        },
+
+        // 绘制第三道波浪
+        runThreeWave(offsetX) {
+            console.log(offsetX)
+
+            this.ctx.beginPath()
+            // 绘制上波浪
+            for (let i = 0; i < 5; i++) {
+                this.ctx.moveTo(offsetX + i * 320, 60)
+                this.ctx.quadraticCurveTo(offsetX + i * 320 + 80, 55, offsetX + i * 320 + 160, 60)
+            }
+
+            // 绘制下波浪
+            for (let i = 0; i < 5; i++) {
+                this.ctx.moveTo(offsetX + i * 320 + 160, 60)
+                this.ctx.quadraticCurveTo(offsetX + i * 320 + 240, 65, offsetX + i * 320 + 320, 60)
+            }
+
+            // 绘制矩形封闭
+            this.ctx.lineTo(this.data.windowWidth, 300)
+            this.ctx.lineTo(0, 300)
+            this.ctx.lineTo(0, 60)
+            this.ctx.setGlobalAlpha(0.3)
+            this.ctx.fillStyle = "#fff"
+            this.ctx.fill()
+        },
+
+    },
+
+
+})
--- miniprogram/pages/product/product.js
+++ miniprogram/pages/product/product.js
@@ -5,76 +5,135 @@ Page({
 	 * 页面的初始数据
 	 */
 	data: {
-		windowWidth: 0, // 屏幕宽度
-		ctx: '', // canvas实例对象
-		offsetX: 0, // canvas波浪图向左偏移的量
-		interval: null, // 定时器
+		// windowWidth: 0, // 屏幕宽度
+		// ctx: '', // canvas实例对象
+		// offsetX1: 0, // canvas波浪图向左偏移的量
+		// offsetX2: 0, // canvas波浪图向左偏移的量
+		// offsetX3: 0, // canvas波浪图向左偏移的量
+		// interval: null, // 定时器
 	},
 
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
 	onLoad: function (options) {
-		// 获取设备信息
-		wx.getSystemInfo({
-			success: (res) => {
-				console.log(res)
-				this.data.windowWidth = res.windowWidth;
-			},
-		});
-		// 初始化canvas画布
-		this.canvasInit();
+		// // 获取设备信息
+		// wx.getSystemInfo({
+		// 	success: (res) => {
+		// 		console.log(res)
+		// 		this.data.windowWidth = res.windowWidth;
+		// 	},
+		// });
+		// // 初始化canvas画布
+		// this.canvasInit();
 	},
 
-	// 初始化canvas画布
-	canvasInit() {
-		this.ctx = wx.createCanvasContext('waveCanvas');
-	},
+	// // 初始化canvas画布
+	// canvasInit() {
+	// 	this.ctx = wx.createCanvasContext('waveCanvas');
+	// },
 
-	// 执行定时器
-	runInterval() {
-		this.data.interval = setInterval(() => {
-			this.data.offsetX = this.data.offsetX -= 4
-			if (this.data.offsetX <= -320) {
-				this.data.offsetX = 0
-			}
-			// 执行绘制
-			this.runWave(this.data.offsetX)
-		}, 50)
-	},
+	// // 执行定时器
+	// runInterval() {
+	// 	this.data.interval = setInterval(() => {
+	// 		this.data.offsetX1 = this.data.offsetX1 -= 6
+	// 		if (this.data.offsetX1 <= -320) {
+	// 			this.data.offsetX1 = 0
+	// 		}
+	// 		this.data.offsetX2 = this.data.offsetX2 -= 4
+	// 		if (this.data.offsetX2 <= -320) {
+	// 			this.data.offsetX2 = 0
+	// 		}
+	// 		this.data.offsetX3 = this.data.offsetX3 -= 1
+	// 		if (this.data.offsetX3 <= -320) {
+	// 			this.data.offsetX3 = 0
+	// 		}
 
-	// 执行绘制
-	runWave(offsetX) {
-		console.log(offsetX)
-	
-		// 绘制上波浪
-		// this.ctx.setGlobalAlpha(0.1)
-		for (let i = 0; i < 5; i++) {
-			this.ctx.moveTo(offsetX + i * 320, 70)
-			this.ctx.quadraticCurveTo(offsetX + i * 320 + 80, 60, offsetX + i * 320 + 160, 70)
-			// this.ctx.strokeStyle = '#58c9fd'
-			// this.ctx.stroke()
-		}
+	// 		// 执行绘制
+	// 		this.runOneWave(this.data.offsetX1)
+	// 		this.runTowWave(this.data.offsetX2)
+	// 		this.runThreeWave(this.data.offsetX3)
+	// 		this.ctx.draw()
+	// 	}, 50)
+	// },
 
-		// 绘制下波浪
-		// this.ctx.setGlobalAlpha(0.1)
-		for (let i = 0; i < 5; i++) {
-			this.ctx.moveTo(offsetX + i * 320 + 160, 70)
-			this.ctx.quadraticCurveTo(offsetX + i * 320 + 240, 80, offsetX + i * 320 + 320, 70)
-			// this.ctx.strokeStyle = '#000'
-			// this.ctx.stroke()
-		}
+	// // 绘制第一道波浪
+	// runOneWave(offsetX) {
+	// 	// console.log(offsetX)
+		
+	// 	this.ctx.beginPath()
+	// 	// 绘制上波浪
+	// 	for (let i = 0; i < 5; i++) {
+	// 		this.ctx.moveTo(offsetX + i * 320, 70)
+	// 		this.ctx.quadraticCurveTo(offsetX + i * 320 + 80, 55, offsetX + i * 320 + 160, 70)
+	// 	}
 
-		// 绘制矩形封闭
-		this.ctx.lineTo(this.data.windowWidth, 300)
-		this.ctx.lineTo(0, 300)
-		this.ctx.lineTo(0, 70)
-		this.ctx.setGlobalAlpha(0.3)
-		this.ctx.fillStyle = "#58c9fd"
-		this.ctx.fill()
+	// 	// 绘制下波浪
+	// 	for (let i = 0; i < 5; i++) {
+	// 		this.ctx.moveTo(offsetX + i * 320 + 160, 70)
+	// 		this.ctx.quadraticCurveTo(offsetX + i * 320 + 240, 85, offsetX + i * 320 + 320, 70)
+	// 	}
 
-		this.ctx.draw()
-	},
+	// 	// 绘制矩形封闭
+	// 	this.ctx.lineTo(this.data.windowWidth, 300)
+	// 	this.ctx.lineTo(0, 300)
+	// 	this.ctx.lineTo(0, 70)
+	// 	this.ctx.setGlobalAlpha(0.9)
+	// 	this.ctx.fillStyle = "#fff"
+	// 	this.ctx.fill()
+	// },
+
+	// // 绘制第二道波浪
+	// runTowWave(offsetX) {
+	// 	console.log(offsetX)
+
+	// 	this.ctx.beginPath()
+	// 	// 绘制上波浪
+	// 	for (let i = 0; i < 5; i++) {
+	// 		this.ctx.moveTo(offsetX + i * 320, 65)
+	// 		this.ctx.quadraticCurveTo(offsetX + i * 320 + 80, 55, offsetX + i * 320 + 160, 65)
+	// 	}
+
+	// 	// 绘制下波浪
+	// 	for (let i = 0; i < 5; i++) {
+	// 		this.ctx.moveTo(offsetX + i * 320 + 160, 65)
+	// 		this.ctx.quadraticCurveTo(offsetX + i * 320 + 240, 75, offsetX + i * 320 + 320, 65)
+	// 	}
+
+	// 	// 绘制矩形封闭
+	// 	this.ctx.lineTo(this.data.windowWidth, 300)
+	// 	this.ctx.lineTo(0, 300)
+	// 	this.ctx.lineTo(0, 65)
+	// 	this.ctx.setGlobalAlpha(0.6)
+	// 	this.ctx.fillStyle = "#fff"
+	// 	this.ctx.fill()
+	// },
+
+	// // 绘制第三道波浪
+	// runThreeWave(offsetX) {
+	// 	console.log(offsetX)
+
+	// 	this.ctx.beginPath()
+	// 	// 绘制上波浪
+	// 	for (let i = 0; i < 5; i++) {
+	// 		this.ctx.moveTo(offsetX + i * 320, 60)
+	// 		this.ctx.quadraticCurveTo(offsetX + i * 320 + 80, 55, offsetX + i * 320 + 160, 60)
+	// 	}
+
+	// 	// 绘制下波浪
+	// 	for (let i = 0; i < 5; i++) {
+	// 		this.ctx.moveTo(offsetX + i * 320 + 160, 60)
+	// 		this.ctx.quadraticCurveTo(offsetX + i * 320 + 240, 65, offsetX + i * 320 + 320, 60)
+	// 	}
+
+	// 	// 绘制矩形封闭
+	// 	this.ctx.lineTo(this.data.windowWidth, 300)
+	// 	this.ctx.lineTo(0, 300)
+	// 	this.ctx.lineTo(0, 60)
+	// 	this.ctx.setGlobalAlpha(0.3)
+	// 	this.ctx.fillStyle = "#fff"
+	// 	this.ctx.fill()
+	// },
 
 	// 手持弹幕
 	toBarrageModule() {
@@ -95,7 +154,7 @@ Page({
 	 */
 	onShow: function () {
 		// 执行定时器
-		this.runInterval();
+		// this.runInterval();
 	},
 
 	/**
@@ -103,7 +162,7 @@ Page({
 	 */
 	onHide: function () {
 		// 清除定时器
-		clearInterval(this.data.interval)
+		// clearInterval(this.data.interval)
 	},
 
 	/**
